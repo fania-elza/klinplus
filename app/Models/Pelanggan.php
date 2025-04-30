b7310ebf383f2e9cd77881e1d30752d3b1307b70
Fania Elza Febriyana <145903776+fania-elza@users.noreply.github.com>
2025-04-30 12:59:01 +0700

    Update Pelanggan.php

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Pelanggan extends Model
 {
     //
+    protected $fillable = [
+        'id_pelanggan',
+        'nama_pelanggan',
+        'nomor_telepon',
+        'email',
+        'alamat',
+        'kota',
+        'gmaps',
+        'patokan'
+    ];
+    
+    protected $primaryKey = 'id_pelanggan';
+    public $incrementing = false;
+    protected $keyType = 'string';
 }
+
+
+
