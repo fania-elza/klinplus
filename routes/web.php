9f686206afc38a99cd566463c309e330b779ae8b
Fania Elza Febriyana <145903776+fania-elza@users.noreply.github.com>
2025-04-30 13:08:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\LayananController;
+use App\Http\Controllers\PetugasController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Api\KotaController;
 
+// Route dasar
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/layanan', function () {
-  return view('layanan.index');
-})->name('layanan');
+// Route untuk testing
+Route::get('/cek', function () {
+    return view('coba');
+});
 
-Route::get('/order', function () {
-   return view('order');
-})->name('order');
+// Route untuk view statis
+Route::get('/layanan/view', function () {
+    return view('layanan.index');
+})->name('layanan.view');
+
+Route::resource('orders', OrderController::class);
 
 Route::get('/jadwal', function () {
-   return view('jadwal');
+    return view('jadwal.index');
 })->name('jadwal');
 
 Route::get('/pembayaran', function () {
-   return view('pembayaran');
+    return view('pembayaran.index');
 })->name('pembayaran');
 
 Route::get('/riwayat', function () {
-   return view('riwayat');
+    return view('riwayat.index');
 })->name('riwayat');
 
+// Route resource
 Route::resource('pelanggan', PelangganController::class);
+Route::resource('layanan', LayananController::class);
+Route::resource('petugas', PetugasController::class)
+    ->parameters(['petugas' => 'petugas:id_petugas']);
 
-Route::get('/petugas', function () {
-   return view('petugas.index');
-})->name('petugas');
+
+Route::get('/kota', [KotaController::class, 'index']);
